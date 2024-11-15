0b5fdab207a4118a25773755db8db1581f953c26
daheePark <qkrekgml7414@naver.com>
2024-11-15 17:04:56 +0900

    init project

---
+++ src/main/java/com/momenty/MomentyApplication.java
@@ -0,0 +1,13 @@
+package com.momenty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momenty/MomentyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momenty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
