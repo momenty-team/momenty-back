73a011ea3bd9302a65b3159097f317de064de030
daheePark <qkrekgml7414@naver.com>
2025-02-26 16:15:10 +0900

    feat: cors 설정 추가

--- src/main/java/com/momenty/global/config/application/ApplicationConfig.java
+++ src/main/java/com/momenty/global/config/application/ApplicationConfig.java
@@ -3,10 +3,26 @@ package com.momenty.global.config.application;
 
 import com.momenty.global.auth.oauth.apple.domain.AppleAuthProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableConfigurationProperties(AppleAuthProperty.class)
 public class ApplicationConfig {
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("https://appleid.apple.com");
+        configuration.addAllowedMethod("POST");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/auth/apple/callback", configuration);
+        return source;
+    }
 }
