936207564bb47fc3b6e93fa63c4570b38fc0c25c
daheePark <qkrekgml7414@gmail.com>
2025-03-02 21:51:28 +0900

    fix: apple token 요청 수정

--- src/main/java/com/momenty/global/auth/oauth/apple/controller/AppleClient.java
+++ src/main/java/com/momenty/global/auth/oauth/apple/controller/AppleClient.java
@@ -4,10 +4,9 @@ import com.momenty.global.auth.oauth.apple.dto.ApplePublicKeyResponse;
 import com.momenty.global.auth.oauth.apple.dto.AppleTokenResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "uniqueAppleClient", url = "https://appleid.apple.com")
 public interface AppleClient {
@@ -16,5 +15,11 @@ public interface AppleClient {
     ApplePublicKeyResponse getAppleAuthPublicKey();
 
     @PostMapping(value = "/auth/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    AppleTokenResponse getAppleToken(@RequestBody MultiValueMap<String, String> request);
+    AppleTokenResponse getAppleToken(
+            @RequestParam("client_id") String clientId,
+            @RequestParam("redirect_uri") String redirectUri,
+            @RequestParam("code") String code,
+            @RequestParam("client_secret") String clientSecret,
+            @RequestParam("grant_type") String grantType
+    );
 }
