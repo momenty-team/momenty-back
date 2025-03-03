1fb3171ff53b684e1b9ff55fb9200b78e2889e82
daheePark <qkrekgml7414@gmail.com>
2025-03-03 18:34:06 +0900

    refactor: redis cofig 수정

--- src/main/java/com/momenty/global/config/common/CommonConfig.java
+++ src/main/java/com/momenty/global/config/common/CommonConfig.java
@@ -1,4 +1,4 @@
-package com.momenty.config.common;
+package com.momenty.global.config.common;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
