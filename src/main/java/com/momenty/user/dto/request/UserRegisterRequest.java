3c0c14b3cddac23e08b8e356fe44c91515099d4c
daheePark <qkrekgml7414@gmail.com>
2025-03-21 00:25:01 +0900

    refactor: 추가 회원가입 api에서 이름 받도록 수정

--- src/main/java/com/momenty/user/dto/request/UserRegisterRequest.java
+++ src/main/java/com/momenty/user/dto/request/UserRegisterRequest.java
@@ -7,9 +7,18 @@ import com.momenty.user.domain.Gender;
 import com.momenty.user.domain.User;
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @JsonNaming(SnakeCaseStrategy.class)
 public record UserRegisterRequest(
+
+        @NotBlank(message = "first name은 필수입니다.")
+        String firstName,
+
+        @NotBlank(message = "last name은 필수입니다.")
+        String lastName,
+
         @NotBlank(message = "닉네임은 필수입니다.")
         String nickname,
 
@@ -20,6 +29,7 @@ public record UserRegisterRequest(
 ) {
     public void applyTo(User user) {
         user.updateProfileToRegister(
+                extractNameFromFullName(firstName, lastName),
                 this.nickname,
                 this.birthDate,
                 Gender.valueOf(this.gender.toUpperCase())
@@ -32,5 +42,12 @@ public record UserRegisterRequest(
                 .birthDate(this.birthDate)
                 .gender(Gender.valueOf(this.gender.toUpperCase()))
                 .build();
+    }
+
+    private String extractNameFromFullName(String firstName, String lastName) {
+        if (firstName == null) firstName = "";
+        if (lastName == null) lastName = "";
+
+        return (firstName + lastName).replaceAll("\\s+", "");
     }
 }
