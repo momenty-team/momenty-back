2deec5580584c92b191d29e69ddffb63878809bf
daheePark <qkrekgml7414@gmail.com>
2025-04-06 00:51:07 +0900

    refactor: 기록 디테일 목록 조회 api에서 기록 디테일 없어도 기록 정보는 반환하도록 수정

--- src/main/java/com/momenty/record/dto/RecordDetailDto.java
+++ src/main/java/com/momenty/record/dto/RecordDetailDto.java
@@ -1,12 +1,10 @@
 package com.momenty.record.dto;
 
 import com.momenty.record.domain.RecordDetail;
-import com.momenty.record.domain.UserRecord;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record RecordDetailDto(
-        UserRecord record,
         Integer id,
         List<String> content,
         Boolean isPublic,
@@ -16,7 +14,6 @@ public record RecordDetailDto(
 
     public static RecordDetailDto of (RecordDetail recordDetails, List<String> content) {
         return new RecordDetailDto(
-                recordDetails.getRecord(),
                 recordDetails.getId(),
                 content,
                 recordDetails.getIsPublic(),
