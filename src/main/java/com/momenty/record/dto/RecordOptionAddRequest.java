dda0322749e82f891b28b1f3a4f0a819ff9a0775
daheePark <qkrekgml7414@gmail.com>
2025-04-06 09:16:19 +0900

    refactor: 옵션 추가 api에서 옵션을 리스트로 받도록 수정

--- src/main/java/com/momenty/record/dto/RecordOptionAddRequest.java
+++ src/main/java/com/momenty/record/dto/RecordOptionAddRequest.java
@@ -2,12 +2,13 @@ package com.momenty.record.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.List;
 
 @JsonNaming(SnakeCaseStrategy.class)
 public record RecordOptionAddRequest(
-        @NotBlank(message = "옵션은 필수입니다.")
-        String option
+        @Size(min = 1, message = "옵션 리스트는 최소 하나 이상이어야 합니다.")
+        List<String> options
 ) {
 
 }
