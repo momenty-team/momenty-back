0ff52417ced831368c4da07ff992b2e7e100fd3a
daheePark <qkrekgml7414@naver.com>
2025-05-25 21:52:47 +0900

    refactor: 고양이 상태 옵션 수정

--- src/main/java/com/momenty/character/dto/CharacterPromptMessage.java
+++ src/main/java/com/momenty/character/dto/CharacterPromptMessage.java
@@ -10,16 +10,18 @@ public enum CharacterPromptMessage {
     CHOOSE_STATUS("위의 HealthKit와 사용자 기록 데이터를 바탕으로 아래의 상태 선택지 중에 가장 어울리는 선택지를 정확하게 딱 한개만 골라 그것만 말해주세요. 그외의 말은 절대 하지 말아주세요.\n"),
     CHARACTER_STATUS_OPTIONS(
             """
-                    지쳤어
-                    스트레스 받아
-                    게을러
-                    무기력해
-                    과로했어
-                    혼잡해
-                    아파
-                    졸려
-                    슬퍼
-                    회복 중이야
+                    Healthy
+                    Energy
+                    Peaceful
+                    Good rest
+                    Soso
+                    Stretching
+                    Rest
+                    Little tired
+                    Tired
+                    Stress
+                    Lazy
+                    Lethargy
             """
     ),
     ;
